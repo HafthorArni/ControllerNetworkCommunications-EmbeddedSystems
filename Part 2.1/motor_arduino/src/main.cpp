b0e177a86c803f59ba41ea6092064c45d3bfa92b
HafthorArni <111596842+HafthorArni@users.noreply.github.com>
2023-11-11 14:42:28 +0000

    changed motor code, not using the states. Should change ref speed if MO sent

--- Part 2.1/motor_arduino/src/main.cpp
+++ Part 2.1/motor_arduino/src/main.cpp
@@ -8,27 +8,25 @@
 #include "PI_controller.h"
 // #include "P_controller.h"
 
-
 Timer_msec timer;
-Digital_out motorIN2(0); //         pin D8
-Analog_out motorIN1(1);  // PWM     pin D9
+Digital_out motorIN2(0); // pin D8
+Analog_out motorIN1(1);  // PWM pin D9
 Encoder encoder(3, 4);   // encoder pin D11 D12
 float kp = 0.01;
 float ti = 5;
-PI_controller controller(kp,ti);
+PI_controller controller(kp, ti);
 // P_controller controller(kp);
 
-
 unsigned long lastPrintTime = 0;
-
 int analogPin = A3; 
 double ref = 0;
 double actual = 0;
 double pwmValue = 0;
 double u = 0;
+bool isRefHigh = false; // New variable to track the state of ref
 
-int main(){  
-    init();// Initialize Arduino framework
+int main() {  
+    init(); // Initialize Arduino framework
     Serial.begin(9600, SERIAL_8N1);  
     timer.init(0.1); // ms
     sei();  // enable interrupts
@@ -39,57 +37,42 @@ int main(){
     motorIN1.set(0);  // duty cycle
     motorIN2.set_lo();
     
-    while(1){
+    while (1) {
         String command;
         char buffer[100]; 
-        // ref = (analogRead(analogPin)/1023.0)*120;
         command = Serial.readStringUntil('\0'); // C strings end with \0
-        // if (millis() - lastPrintTime >= 250) { 
-        if(command.substring(0,4) == "LED "){   // begins with "LED "?
-          String intString = command.substring(4, command.length());
-          int ref = intString.toInt();
-          // int level = msg[1];       // extract the int
-          if(ref>=0 && ref<=255){          // is it in range?
-            actual = abs(encoder.speed());
-            u = controller.update(ref, actual);
-            u = constrain(u, 0.0, 0.999); // Ensure pwmValue is within [0, 1]
-            pwmValue = u;
-            motorIN1.set(pwmValue);
-          }
-          else{                                // no, error message back
-              sprintf(buffer, "MO :Error: %d is out of range\n", ref);
-          } 
+        if (command.substring(0, 2) == "MO") {   // begins with "MO"?
+            if (!isRefHigh) {
+                ref = 100;
+                isRefHigh = true;
+            } else {
+                ref = 0;
+                isRefHigh = false;
+            }
+            sprintf(buffer, "MO :Reference set to %d\n", (int)ref);
+        } else {
+            sprintf(buffer, "MO :Unknown command: %s", command.c_str());
         }
-        else{ sprintf(buffer, "MO :Unknown command: %s", command.c_str()); }
-        Serial.print(buffer);                 // send the buffer to the RPi
-          // Serial.print("speed: (");
-          // Serial.print("Ref: ");
-          // Serial.print(ref);
-          // Serial.print(" - Act: ");
-          // Serial.print(actual);
-          // Serial.print(") [RPM], ");
-          // Serial.print(" duty cycle: ");
-          // Serial.print(pwmValue);
-          // Serial.print(" u: ");
-          // Serial.println(u);
-          // lastPrintTime = millis();  
-      // }
-      encoder.update();
-      _delay_ms(3);
-  }
+        Serial.print(buffer); // send the buffer to the RPi
+        actual = abs(encoder.speed());
+        u = controller.update(ref, actual);
+        u = constrain(u, 0.0, 0.999); // Ensure pwmValue is within [0, 1]
+        pwmValue = u;
+        motorIN1.set(pwmValue);
+        encoder.update();
+        _delay_ms(3);
+    }
     return 0;
 }
 
-ISR (INT0_vect){
+ISR (INT0_vect) {
     encoder.update();
 }
 
-ISR(TIMER1_COMPA_vect){
-    // encoder.update();
+ISR(TIMER1_COMPA_vect) {
     motorIN1.pin.set_hi();      
 }
 
-ISR(TIMER1_COMPB_vect){
-    // encoder.update();
+ISR(TIMER1_COMPB_vect) {
     motorIN1.pin.set_lo(); 
 }
